0546d605d470c32a0410d1e8cba790caa8c6449e
naveengus <naveen.g13579@gmail.com>
2024-04-26 20:44:41 +0530

    calculator-task8

---
+++ script.js
@@ -0,0 +1,33 @@
+let screen = document.getElementById("display");
+let screenValue = screen.value;
+
+function inputNum(d) {
+  screenValue = screenValue === "0" ? d : screenValue + d;
+  updateScreen();
+}
+
+function inputdot() {
+  if (!screenValue.includes(".")) {
+    screenValue = screenValue + ".";
+  }
+  updateScreen();
+}
+
+function clearEnter() {
+  screenValue = screenValue.slice(0, -1);
+  updateScreen();
+}
+function allClear() {
+  screenValue = "";
+  updateScreen();
+}
+
+function equalval() {
+  screenValue = eval(screenValue);
+  updateScreen();
+}
+
+function updateScreen() {
+  screen.value = screenValue;
+  // console.log(screen.textContent);
+}
